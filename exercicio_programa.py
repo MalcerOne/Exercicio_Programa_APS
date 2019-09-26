e5e271a1df2768ce6d93dbd6b4cfebb03a1619df
anatofetti <55840524+anatofetti@users.noreply.github.com>
2019-09-26 18:10:45 -0300

    Update exercicio_programa.py

--- exercicio_programa.py
+++ exercicio_programa.py
@@ -4,7 +4,20 @@ def intro():
     print("♠♣ Bem vindo ao Blackjack! ♥♦\n"
           "Aproxime-se e veja se você consegue\n"
           "ganhar do dealer e chegar o mais perto de 21 possível sem estourar!\n\n"
-          "As regras são as seguintes:\n"
+          "As regras do Blackjack são as seguintes:\n"
+          '--> O dealer te entrega duas cartas. Se a soma delas for exatamente 21,\n'
+          'é Blackjack, e você ganha automaticamente!'
+          "--> Se não há Blackjack, você tem duas opções:\n"
+          '    --> Parar o jogo com as cartas que possui\n'
+          '    --> Pedir mais cartas, uma por vez, até decidir parar o jogo\n'
+          '--> Se a soma de suas cartas for maior que 21, você perde automaticamente\n'
+          '--> Se você ainda está no jogo, suas cartas são comparadas com a do dealer,\n'
+          'e quem estiver mais próximo de 21 ganha a rodada. Se o dealer estourar 21/n'
+          'pontos, você ganha também.\n\n'
+          'As recompensas são as seguintes:\n'
+          '--> Ganhar com Blackjack: retorno da aposta, mais 150% do seu valor\n'
+          '--> Ganhar: retorno da aposta, mais 100% do seu valor\n\n'
+          "As regras do casino são as seguintes:\n"
           "--> Não aposte o que você não tem em sua carteira. Erros são aceitos,\n"
           "mas se você repeti-los por mais de cinco vezes, os seguranças do cassino\n"
           "te chutam para fora.\n"
@@ -21,9 +34,14 @@ naipe_ouros = ["Ao", "2o", "3o", "4o", "5o", "6o", "7o", "8o", "9o", "10o", "Jo"
 
 baralho = [naipe_copas, naipe_espadas, naipe_ouros, naipe_paus]
 
-
-carteira  = int(input("Quanto voce quer gastar de dinheiro? (essa será sua carteira de apostas): "))
-
+carteira  = int(input("Quanto você quer gastar de dinheiro? (essa será sua carteira de apostas): "))
+while carteira <=0 or carteira>1000000:
+    if carteira<=0:
+        print('Insira um valor de dinheiro verdadeiro')
+    else:
+        print('O casino possui um limite de R$1.000.000 por participante')
+    carteira  = int(input("Quanto você quer gastar de dinheiro? (essa será sua carteira de apostas): "))
+    
 i = True
 while i:
     aposta = int(input("Quanto você quer apostar?: "))
