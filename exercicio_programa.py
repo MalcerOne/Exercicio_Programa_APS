93fa259e23b2114003bde4a0ea8cd8c354affc5f
Rafael Malcervelli <48256253+MalcerOne@users.noreply.github.com>
2019-09-27 07:46:45 -0300

    Update exercicio_programa.py

--- exercicio_programa.py
+++ exercicio_programa.py
@@ -1,5 +1,5 @@
 import random
-#Introducao do jogo para os players, alem de mostrar as regras do mesmo.
+# Introducao do jogo para os players, alem de mostrar as regras do mesmo.
 def intro():
     print("♠♣ Bem vindo ao Blackjack! ♥♦\n"
           "Aproxime-se e veja se você consegue\n"
@@ -24,9 +24,11 @@ def intro():
           "--> Quando você fica zerado, o jogo acaba, e boa sorte da próxima vez!\n"
           "Ah, claro, você pode digitar 'fim' a qualquer momento que quiser sair.\n"
           "--> Se divirta!")
+
+
 intro()
-    
-#Criacao dos baralhos
+
+# Criacao dos baralhos
 naipe_paus = ["Ap", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "Jp", "Qp", "Kp"]
 naipe_copas = ["Ac", "2c", "3c", "4c", "5c", "6c", "7c", "8c", "9c", "10c", "Jc", "Qc", "Kc"]
 naipe_espadas = ["Ae", "2e", "3e", "4e", "5e", "6e", "7e", "8e", "9e", "10e", "Je", "Qe", "Ke"]
@@ -41,7 +43,7 @@ dificuldade_jogo = input("Qual a dificuldade do jogo? (mf, f, m, d): ")
 while dificuldade_jogo != "mf" and dificuldade_jogo != "f" and dificuldade_jogo != "m" and dificuldade_jogo != "d":
     print("Desculpe, dificuldade não encontrada. Tente de novo.")
     dificuldade_jogo = input("Qual a dificuldade do jogo? (mt, f, m, d): ")
-    
+
 # Conforme a dificuldade aumenta, a disponibilidade de cartas fica mais escassa.
 if dificuldade_jogo == "mf":
     print("Ridículo.")
@@ -54,16 +56,16 @@ elif dificuldade_jogo == "m":
     numero_de_baralhos = 3
 elif dificuldade_jogo == "d":
     print("Boa sorte.")
-   
-# Recebe o quanto o jogador quer colocar na carteira.                          
-carteira  = int(input("Quanto você quer gastar de dinheiro? (essa será sua carteira de apostas): "))
-while carteira <=0 or carteira>1000000:
-    if carteira<=0:
+
+# Recebe o quanto o jogador quer colocar na carteira.
+carteira = int(input("Quanto você quer gastar de dinheiro? (essa será sua carteira de apostas): "))
+while carteira <= 0 or carteira > 1000000:
+    if carteira <= 0:
         print('Insira um valor de dinheiro verdadeiro')
     else:
         print('O casino possui um limite de R$1.000.000 por participante')
-    carteira  = int(input("Quanto você quer gastar de dinheiro? (essa será sua carteira de apostas): "))
-    
+    carteira = int(input("Quanto você quer gastar de dinheiro? (essa será sua carteira de apostas): "))
+
 # Loop do jogo.
 i = True
 while i:
@@ -73,7 +75,12 @@ while i:
         break
     contador_seguranca = 0
     while aposta > carteira or aposta <= 0:
+        if contador_seguranca == 5:
+            print("Nos avisamos. Os seguranças cuidarão de você.")
+            i = False
+            break
         contador_seguranca += 1
         print("Você não tem esse dinheiro! Aposte de novo. Você tem mais {0} tentativas\n"
-              "antes de ser expulso da mesa. ".format(5-contador_seguranca))
+              "antes de ser expulso da mesa. ".format(5 - contador_seguranca))
         aposta = int(input("Quanto você quer apostar?: "))
+
