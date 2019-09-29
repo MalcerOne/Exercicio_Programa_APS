bf9ab8c83cba5538c9b1c8514442cba5138f2b92
anatofetti <55840524+anatofetti@users.noreply.github.com>
2019-09-29 19:02:08 -0300

    Update exercicio_programa.py

--- exercicio_programa.py
+++ exercicio_programa.py
@@ -2,21 +2,21 @@ import random
 # Introducao do jogo para os players, alem de mostrar as regras do mesmo.
 def intro():
     print("♠♣ Bem vindo ao Blackjack! ♥♦\n"
-          "Aproxime-se e veja se você consegue ganhar do dealer e chegar o mais perto\n"
-          "de 21 possível sem estourar!\n\n"
+          "Aproxime-se e veja se você consegue ganhar do dealer e chegar o mais\n"
+          "perto de 21 possível sem estourar!\n\n"
           "As regras do Blackjack são as seguintes:\n"
-          '--> O dealer te entrega duas cartas. Se a soma delas for exatamente 21,\n'
-          'é Blackjack, e você ganha automaticamente!\n'
+          '--> O dealer te entrega duas cartas. Se a soma delas for exatamente\n'
+          '21, é Blackjack, e você ganha automaticamente!\n'
           "--> Se não há Blackjack, você tem duas opções:\n"
           '    --> Parar o jogo com as cartas que possui\n'
           '    --> Pedir mais cartas, uma por vez, até decidir parar o jogo\n'
-          '--> Se a soma de suas cartas for maior que 21, você perde automaticamente\n'
-          '--> Se você ainda está no jogo, suas cartas são comparadas com a do dealer,\n'
-          'e quem estiver mais próximo de 21 ganha a rodada. Se o dealer estourar 21\n'
-          'pontos, você ganha também.\n\n'
+          '--> Se a soma de suas cartas for maior que 21, você perde na hora\n'
+          '--> Se você ainda está no jogo, suas cartas são comparadas com a do\n'
+          'dealer, e quem estiver mais próximo de 21 ganha a rodada. Se o dealer\n'
+          'estourar 21 pontos, você ganha também.\n\n'
           'As recompensas são as seguintes:\n'
-          '--> Ganhar com Blackjack: retorno da aposta, mais 150% do seu valor\n'
-          '--> Ganhar: retorno da aposta, mais 100% do seu valor\n\n'
+          '--> Ganhar com Blackjack: retorno da aposta, mais 150% do seu valor.\n'
+          '--> Ganhar: retorno da aposta, mais 100% do seu valor.\n\n'
           "As regras do casino são as seguintes:\n"
           "--> Não aposte o que você não tem em sua carteira. Erros são aceitos,\n"
           "mas se você repeti-los por mais de cinco vezes, os seguranças do cassino\n"
@@ -29,13 +29,9 @@ def intro():
 intro()
 
 # Criacao dos baralhos
-naipe_paus = ["Ap", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "Jp", "Qp", "Kp"]
-naipe_copas = ["Ac", "2c", "3c", "4c", "5c", "6c", "7c", "8c", "9c", "10c", "Jc", "Qc", "Kc"]
-naipe_espadas = ["Ae", "2e", "3e", "4e", "5e", "6e", "7e", "8e", "9e", "10e", "Je", "Qe", "Ke"]
-naipe_ouros = ["Ao", "2o", "3o", "4o", "5o", "6o", "7o", "8o", "9o", "10o", "Jo", "Qo", "Ko"]
 
 numero_de_baralhos = 1
-baralho = [naipe_copas, naipe_espadas, naipe_ouros, naipe_paus] * numero_de_baralhos
+baralho_base = ["Ac", "2c", "3c", "4c", "5c", "6c", "7c", "8c", "9c", "10c", "Jc", "Qc", "Kc","Ae", "2e", "3e", "4e", "5e", "6e", "7e", "8e", "9e", "10e", "Je", "Qe", "Ke","Ao", "2o", "3o", "4o", "5o", "6o", "7o", "8o", "9o", "10o", "Jo", "Qo", "Ko","Ap", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "Jp", "Qp", "Kp"]
 
 # Dificuldade do jogo
 dificuldade_jogo = input("Qual a dificuldade do jogo? (mf, f, m, d): ")
@@ -75,12 +71,37 @@ while i:
         break
     contador_seguranca = 0
     while aposta > carteira or aposta <= 0:
-        if contador_seguranca == 5:
-            print("Nos avisamos. Os seguranças cuidarão de você.")
+        if contador_seguranca == 4:
+            print("Nós avisamos. Os seguranças cuidarão de você.")
             i = False
             break
         contador_seguranca += 1
-        print("Você não tem esse dinheiro! Aposte de novo. Você tem mais {0} tentativas\n"
-              "antes de ser expulso da mesa. ".format(5 - contador_seguranca))
+        if contador_seguranca == 4:
+            print("Você não tem esse dinheiro! Aposte de novo. É sua última tentativa\n"
+                  "antes de ser expulso da mesa. "
+        else:
+            print("Você não tem esse dinheiro! Aposte de novo. Você tem mais {0} tentativas\n"
+                  "antes de ser expulso da mesa. ".format(5 - contador_seguranca))
         aposta = int(input("Quanto você quer apostar?: "))
 
+#Definindo as cartas que entram no jogo
+    baralho = []
+    for t in range(0,numero_de_baralhos):
+        for a in range(len(baralho_base)):
+            baralho.append(baralho_base[a])
+            
+#Embaralhando as cartas
+    monte=[]
+    n=len(baralho)
+    while n!= 0:
+        carta=baralho[random.randint(0,n-1)]
+        monte.append(carta)
+        b=0
+        while b < n:
+            if carta==baralho[b]:
+                  del baralho[b]
+                  break
+            b+=1
+        n-=1
+
+    i=False
